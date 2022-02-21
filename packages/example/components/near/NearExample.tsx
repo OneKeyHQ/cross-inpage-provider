e4bf74d80da7388de6180a04b7c9025be99da460
zuozhuo <zuoyizhuo@live.com>
2022-02-21 12:39:38 +0800

    fix logger

--- packages/example/components/near/NearExample.tsx
+++ packages/example/components/near/NearExample.tsx
@@ -1,5 +1,9 @@
-import { OneKeyNearProvider, NearAccountsChangedPayload } from '@onekeyfe/onekey-near-provider';
-import { useEffect, useState } from 'react';
+import {
+  OneKeyNearProvider,
+  NearAccountsChangedPayload,
+  NearNetworkChangedPayload,
+} from '@onekeyfe/onekey-near-provider';
+import { useCallback, useEffect, useState } from 'react';
 import * as NearApi from 'near-api-js';
 
 const hasWindow = typeof window !== 'undefined';
@@ -8,6 +12,15 @@ export default function NearExample() {
   const [provider, setProvider] = useState<OneKeyNearProvider | null>(null);
   const [accountId, setAccountId] = useState('');
   const [networkId, setNetworkId] = useState('');
+  const onAccountsChanged = useCallback((payload: NearAccountsChangedPayload) => {
+    const _accountId = payload?.accounts?.[0]?.accountId || '';
+    console.log('onAccountsChanged >>>', _accountId);
+    setAccountId(_accountId);
+  }, []);
+  const onNetworkChanged = useCallback((payload: NearNetworkChangedPayload) => {
+    console.log('onNetworkChanged >>>', payload);
+    setNetworkId(payload.networkId);
+  }, []);
   useEffect(() => {
     if (!hasWindow) {
       // return;
@@ -25,7 +38,7 @@ export default function NearExample() {
       const _provider = new OneKeyNearProvider({
         connection,
         networkId: config.networkId,
-        logger: console,
+        logger: null,
       });
       const installed = await _provider.detectWalletInstalled();
       if (!installed) {
@@ -33,32 +46,37 @@ export default function NearExample() {
       }
       setAccountId(_provider.getAccountId());
       setNetworkId(_provider.getSelectedNetwork().networkId);
-      _provider.on('accountsChanged', (payload) => {
-        const _accountId = payload?.accounts?.[0]?.accountId || '';
-        console.log('accountsChanged', _accountId);
-        setAccountId(_accountId);
-      });
-      _provider.on('networkChanged', (payload) => {
-        setNetworkId(payload.networkId);
-      });
+
+      _provider.on('accountsChanged', onAccountsChanged);
+      _provider.on('networkChanged', onNetworkChanged);
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       global.$nearWallet = _provider;
       setProvider(_provider);
     })();
-  }, []);
+  }, [onAccountsChanged, onNetworkChanged]);
+  useEffect(() => {
+    return () => {
+      if (provider) {
+        provider.off('accountsChanged', onAccountsChanged);
+        provider.off('networkChanged', onNetworkChanged);
+      }
+    };
+  }, [onAccountsChanged, onNetworkChanged, provider]);
 
   return (
     <div>
-      <div>accountId: {accountId}</div>
-      <div>networkId: {networkId}</div>
-      <div>
-        {accountId ? (
-          <button onClick={() => provider?.signOut()}>signOut</button>
-        ) : (
-          <button onClick={() => provider?.requestSignIn()}>requestSignIn</button>
-        )}
-      </div>
+      {!provider && <a href={'https://onekey.so/plugin'}>Install OneKey Extension →</a>}
+      {provider && (
+        <div>
+          <div>accountId: {accountId}</div>
+          <div>networkId: {networkId}</div>
+          <div>
+            <button onClick={() => provider?.requestSignIn()}>signIn</button>
+            <button onClick={() => provider?.signOut()}>signOut</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

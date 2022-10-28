7092933f65e8c99f1f4febf2387a0c92d2a49e00
morizon <sidmorizon@outlook.com>
2022-10-28 09:11:31 +0800

    fix: webview export

--- packages/webview/src/index.ts
+++ packages/webview/src/index.ts
@@ -1,6 +1,7 @@
-export * from './DesktopWebView';
+// DesktopWebView, NativeWebView moved to monorepo-app
+// export * from './DesktopWebView';
+// export * from './NativeWebView';
 export * from './JsBridgeDesktopHost';
-export * from './NativeWebView';
 export * from './JsBridgeNativeHost';
 export * from './useIsIpcReady';
 export * from './useWebViewBridge';

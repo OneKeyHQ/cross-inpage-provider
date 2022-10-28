74d427c3760d28892f858d2cd903da14b80311d9
morizon <sidmorizon@outlook.com>
2022-10-28 13:18:07 +0800

    fix: new example home page (#65)
    * fix: example home
    
    * fix: add icons
    
    * fix: close icon

--- packages/example/pages/index.tsx
+++ packages/example/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { RefObject, useMemo } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 // import Image from 'next/image';
@@ -7,10 +7,195 @@ import styles from '../styles/Home.module.css';
 import packageJson from '../package.json';
 import { useEffect } from 'react';
 import { Button } from 'native-base';
+import { Tree, NodeApi } from 'react-arborist';
+import * as uuid from 'uuid';
 
 // const myImageLoader = ({ src, width, quality }: any) => {
 //   return src as string;
 // };
+
+type ITreeNodeInfo = {
+  isLeaf: boolean;
+  isOpen: boolean;
+  isClosed: boolean;
+  isInternal: boolean;
+  data: ITreeNodeData;
+  toggle: () => void;
+};
+type ITreeNodeData = {
+  id: string;
+  name: string;
+  icon?: string;
+  href?: string;
+  target?: string;
+  children?: ITreeNodeData[];
+};
+type ITreeNodeProps = {
+  node: ITreeNodeInfo;
+  style: unknown;
+  dragHandle: RefObject<HTMLDivElement>;
+};
+function TreeNode({ node, style, dragHandle }: ITreeNodeProps) {
+  const { href, target, name, icon } = node.data;
+  const link = useMemo(() => {
+    if (target && href) {
+      return (
+        <a href={href} target={target}>
+          {`${name} →`}
+        </a>
+      );
+    }
+    if (href) {
+      return <Link href={href}>{`${name} →`}</Link>;
+    }
+    return <span>{name}</span>;
+  }, [href, name, target]);
+  const imgIcon = useMemo(() => {
+    if (icon) {
+      return (
+        <img alt={icon} src={icon} style={{ width: '14px', height: '14px', borderRadius: '50%' }} />
+      );
+    }
+    if (node.isInternal) {
+      if (node.isClosed) {
+        return '📁';
+      }
+      return '📂';
+    }
+    return '👉';
+  }, [icon, node.isClosed, node.isInternal]);
+  return (
+    <div style={style} ref={dragHandle} onClick={() => node.toggle()}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          cursor: node.isInternal ? 'pointer' : undefined,
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            minWidth: '16px',
+            marginRight: '8px',
+          }}
+        >
+          {imgIcon}
+        </div>
+        {link}
+      </div>
+    </div>
+  );
+}
+
+const data: ITreeNodeData[] = [
+  {
+    id: uuid.v4(),
+    name: 'General',
+    children: [
+      { id: uuid.v4(), name: 'iframe', href: '/iframe' },
+      { id: uuid.v4(), name: 'DeepLink', href: '/deeplink' },
+      { id: uuid.v4(), name: 'DappList', href: '/dappList' },
+      {
+        id: uuid.v4(),
+        name: 'Hardware SDK (coming soon)',
+        href: '',
+        icon: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/319/mobile-phone_1f4f1.png',
+      },
+    ],
+  },
+  {
+    id: uuid.v4(),
+    name: 'WalletConnect',
+    // icon: 'https://example.walletconnect.org/favicon.ico',
+    children: [
+      {
+        id: uuid.v4(),
+        name: 'WalletConnect V1',
+        href: 'https://example.walletconnect.org',
+        target: 'WalletConnectExampleV1',
+        icon: 'https://example.walletconnect.org/favicon.ico',
+      },
+      {
+        id: uuid.v4(),
+        name: 'WalletConnect V2',
+        href: 'https://react-app.walletconnect.com',
+        target: 'WalletConnectExampleV2',
+        icon: 'https://example.walletconnect.org/favicon.ico',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Aptos WalletConnect',
+        href: '/aptosWalletconnect',
+        icon: 'https://onekey-asset.com/assets/apt/apt.png',
+      },
+    ],
+  },
+  {
+    id: uuid.v4(),
+    name: 'Networks',
+    children: [
+      {
+        id: uuid.v4(),
+        name: 'EVM',
+        href: '/ethereum',
+        icon: 'https://onekey-asset.com/assets/eth/eth.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Solana',
+        href: '/solana',
+        icon: 'https://onekey-asset.com/assets/sol/sol.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'NEAR',
+        href: '/near',
+        icon: 'https://onekey-asset.com/assets/near/near.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'NEAR ref-ui',
+        href: 'https://dapp-near-ref-ui.onekeytest.com',
+        target: '_blank',
+        icon: 'https://onekey-asset.com/assets/near/near.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Starcoin',
+        href: '/starcoin',
+        icon: 'https://onekey-asset.com/assets/stc/stc.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Aptos',
+        href: '/aptos',
+        icon: 'https://onekey-asset.com/assets/apt/apt.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Aptos Martian',
+        href: '/aptosMartian',
+        icon: 'https://onekey-asset.com/assets/apt/apt.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Conflux',
+        href: '/conflux',
+        icon: 'https://onekey-asset.com/assets/cfx/cfx.png',
+      },
+      {
+        id: uuid.v4(),
+        name: 'Tron',
+        href: '/tron',
+        icon: 'https://onekey-asset.com/assets/trx/trx.png',
+      },
+    ],
+  },
+];
 
 const Home: NextPage = () => {
   useEffect(() => {
@@ -25,28 +210,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Link href="/iframe">iframe →</Link>
-        <Link href="/ethereum">EVM →</Link>
-        <Link href="/solana">Solana →</Link>
-        <Link href="/near">NEAR →</Link>
-        <a target="_blank" href="https://dapp-near-ref-ui.onekeytest.com">
-          NEAR ref-ui →
-        </a>
-        <Link href="/starcoin">Starcoin →</Link>
-        <Link href="/aptos">Aptos →</Link>
-        <Link href="/aptosMartian">Aptos Martian →</Link>
-        <Link href="/conflux">Conflux →</Link>
-        <Link href="/tron">Tron →</Link>
-        <a target="WalletConnectExampleV1" href="https://example.walletconnect.org">
-          WalletConnect V1 →
-        </a>
-        <a target="_blank" href="https://react-app.walletconnect.com">
-          WalletConnect V2 →
-        </a>
-        <Link href="/aptosWalletconnect">Aptos WalletConnect →</Link>
-        <Link href="/deeplink">DeepLink →</Link>
-        <Link href="/dappList">Dapp List →</Link>
-        <a>Hardware SDK (coming soon)</a>
+        <Tree initialData={data}>{TreeNode as any}</Tree>
         <Button onPress={() => window.location.reload()}>refresh</Button>
       </main>
 

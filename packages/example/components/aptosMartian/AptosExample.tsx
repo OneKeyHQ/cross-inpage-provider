05609b7c5c05eb7b7dec9d9fd26e7e74c2bb140d
ByteZhang1024 <ByteZhang@protonmail.com>
2022-11-30 20:09:34 +0800

    feat: add msafe multi-signature demo

--- packages/example/components/aptosMartian/AptosExample.tsx
+++ packages/example/components/aptosMartian/AptosExample.tsx
@@ -5,6 +5,8 @@ import { random } from 'lodash';
 
 import { DAppList } from '../dappList/DAppList';
 import { dapps } from '../aptos/dapps.config';
+import { HexString, TxnBuilderTypes, BCS } from "aptos";
+import { TxnPayload } from '@onekeyfe/onekey-aptos-provider/dist/types';
 
 declare global {
   interface Window {
@@ -233,6 +235,71 @@ export default function App() {
     console.log('[getLedgerInfo]', res);
   };
 
+  const msafeMultiSignature = async () => {
+    const addressFormat = (addr: string) => TxnBuilderTypes.AccountAddress.fromHex(addr);
+    const testMsafe = '0xaa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e';
+
+    const serializer = new BCS.Serializer();
+    const owners = [
+      "0x5c7b342e9ee2e582ad16fb602e8ebb6ba39b3bfa02a4fd3865853b10dc75765f",
+      "0xa3f6a53c57395401ce64f09a188e2259dc9b156387e76c88a7a80a8fe5254476",
+    ];
+    BCS.serializeVector(
+      owners.map((owner) => addressFormat(owner)),
+      serializer
+    );
+
+    const payload = new TxnBuilderTypes.TransactionPayloadEntryFunction(
+      TxnBuilderTypes.EntryFunction.natural(
+        `0xaa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e::creator`,
+        "init_wallet_creation",
+        [],
+        [
+          serializer.getBytes(),
+          BCS.bcsSerializeU8(2),
+          BCS.bcsSerializeUint64(10000000),
+          BCS.bcsSerializeBytes(
+            new HexString(
+              "b5e97db07fa0bd0e5598aa3643a9bc6f6693bddc1a9fec9e674a461eaa00b193a527b6487c9ba480a3dbfbc351a3fcafd0a5044a0b3c877f759fa5df64a692f1000000000000000002aa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e0d6d6f6d656e74756d5f7361666508726567697374657200010c0b77616c6c6574206e616d65e02e0000000000007800000000000000fa7984630000000001"
+            ).toUint8Array()
+          ),
+          BCS.bcsSerializeBytes(
+            new HexString(
+              "fc284900723375e6b087a166c04edf6a1b71a361ad671608a849b733a878aaf910150dcf28e0f197675137abb328db6b55a6d98bee53413ad49c16549c1f0701"
+            ).toUint8Array()
+          ),
+        ]
+      )
+    );
+
+    const sn = await provider.getAccount(testMsafe).then(acc => BigInt(acc.sequence_number) + BigInt(1))
+
+    const testTxn = new TxnBuilderTypes.RawTransaction(
+      TxnBuilderTypes.AccountAddress.fromHex(testMsafe),
+      sn,
+      payload,
+      BigInt(20123),
+      BigInt(123),
+      BigInt(1793884475),
+      new TxnBuilderTypes.ChainId(1)
+    );
+    const bcsUnsignedTxn = BCS.bcsToBytes(testTxn);
+    //@ts-expect-error
+    const res: string = await provider.signTransaction(HexString.fromUint8Array(bcsUnsignedTxn).noPrefix());
+    const bcsSignedTxn = Uint8Array.from(res.split(",").map((s) => Number(s)));
+    const signedTransaction = TxnBuilderTypes.SignedTransaction.deserialize(
+      new BCS.Deserializer(bcsSignedTxn)
+    );
+
+    console.log('[msafeMultiSignature] success ', JSON.stringify(signedTransaction, (key, value) =>
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      typeof value === 'bigint'
+        ? value.toString()
+        : value
+    ));
+  };
+
+
   return (
     <div>
       <DAppList dapps={dapps} />
@@ -293,6 +360,12 @@ export default function App() {
                 onChange={(e) => setNftTokenName(e.target.value)}
               /> NFT Name 不能重名，要改名字。</pre>
               <pre><button onClick={createToken}>Create Token</button></pre>
+            </div>
+
+            <br />
+            <div style={{ border: '1px solid #cccccc', flexDirection: 'column' }}>
+              Msafe Multi-signature Demo (Mainnet)
+              <pre><button onClick={msafeMultiSignature}>Sign Transaction</button></pre>
             </div>
           </>
         ) : (

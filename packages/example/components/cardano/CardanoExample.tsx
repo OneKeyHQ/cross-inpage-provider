f6ac45a1f7c002a2f5947f8536776c208b651911
Leon <lixiao.dev@gmail.com>
2022-12-14 10:50:27 +0800

    fix: optimize cardano example

--- packages/example/components/cardano/CardanoExample.tsx
+++ packages/example/components/cardano/CardanoExample.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
 import { ProviderCardano } from '@onekeyfe/onekey-cardano-provider'
 import { dapps } from './dapps.config';
 import { DAppList } from '../dappList/DAppList';
@@ -38,21 +38,23 @@ export default function CardanoExample() {
   const [changeAddress, setChangeAddress] = useState<string>('')
   const [rewardAddresses, setRewardAddresses] = useState<string[]>([])
   const [balance, setBalance] = useState<string>('')
+  const prevAccountRef = useRef<string | null>(null)
 
   const getWalletInfo = useCallback(async () => {
     const api = await provider.onekey.enable()
     try {
       void api.getNetworkId().then((id: number) => setNetworkId(id))
-      void api.getUtxos().then((utxos: string[]) => setUtxos(utxos))
+      void api.getUtxos().then((utxos: string[]) => setUtxos(utxos ?? []))
       void api.getBalance().then((balance: string) => setBalance(balance))
       void api.getUsedAddresses().then((addresses: string[]) => setUsedAddresses(addresses))
       void api.getUnusedAddresses().then((addresses: string[]) => setUnusedAddresses(addresses))
       void api.getRewardAddresses().then((addresses: string[]) => setRewardAddresses(addresses))
       void api.getChangeAddress().then((address: string) => setChangeAddress(address))
-    } catch {
+    } catch (e) {
     // ignore
+      console.log(e)
     }
-  }, [provider])
+  }, [provider?.onekey])
 
 
   useEffect(() => {
@@ -60,7 +62,6 @@ export default function CardanoExample() {
     try {
       provider.on('connect', (address: string) => {
       console.log(`cardanoWallet.on [connect] ${address}`);
-      getWalletInfo().catch(e => console.error(e))
     })
     }	catch (e) {
       console.error(e)
@@ -69,7 +70,11 @@ export default function CardanoExample() {
     try {
       provider.on('accountChanged', (address: string) => {
         console.log(`cardanoWallet.on [accountChange] ${address}`);
-        getWalletInfo().catch(e => console.error(e))
+        if (prevAccountRef.current !== null && prevAccountRef.current !== address) {
+          console.log('account changed ======>>>>> ')
+          getWalletInfo().catch(e => console.error(e))
+        }
+        prevAccountRef.current = address
       });
     } catch (e) {
       console.error(e)
@@ -79,6 +84,15 @@ export default function CardanoExample() {
     // 
     }
   }, [provider, getWalletInfo])
+
+  useEffect(() => {
+    if (connected && API) {
+      setTimeout(() => {
+        getWalletInfo().catch(e => console.error(e))    
+      }, 500)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connected, API])
 
   const connectWallet = async () => {
     try {
@@ -92,11 +106,11 @@ export default function CardanoExample() {
     }
   }
 
-  const signTransaction = async () => {
-    const txHex = '84a300818258200528ed5dd2134534df4d999c9cd712e8b47441a0363cc972bf5d5c72635c3e5100018282583901cd5aa28518568615ed6bd4ad40e6cecd65734373bf578489b02830e38d6dc22f3b6dfd333cf6495e8fb3fffbc2936cc010827b32d92eca1b1a000f424082583901922941c2e490c9f9f8706c66f7f46b5abec243fcb663f02817fb9b83aa618af8973bc5f1fdb9c9db97d1c55f0d4d4a1e633bbb14ab43b4fc1a001449d5021a00029075a0f5f6'
-    const witness = await API.signTx(txHex, true)
-    console.log(witness)
-  }
+  // const signTransaction = async () => {
+  //   const txHex = '84a300818258200528ed5dd2134534df4d999c9cd712e8b47441a0363cc972bf5d5c72635c3e5100018282583901cd5aa28518568615ed6bd4ad40e6cecd65734373bf578489b02830e38d6dc22f3b6dfd333cf6495e8fb3fffbc2936cc010827b32d92eca1b1a000f424082583901922941c2e490c9f9f8706c66f7f46b5abec243fcb663f02817fb9b83aa618af8973bc5f1fdb9c9db97d1c55f0d4d4a1e633bbb14ab43b4fc1a001449d5021a00029075a0f5f6'
+  //   const witness = await API.signTx(txHex, true)
+  //   console.log(witness)
+  // }
 
   const signData = async () => {
     const signature = await API.signData(usedAddresses?.[0], Buffer.from('hello world', 'utf8').toString('hex'))
@@ -118,13 +132,13 @@ export default function CardanoExample() {
             <ul>walletName: <img src={provider.onekey.icon} style={{height: '24px', width: '24px'}} /> {provider.onekey.name}</ul>
             <ul>apiVersion: {provider.onekey.apiVersion}</ul>
             <ul>networkId: {networkId}</ul>
-            <ul style={{width: '80%'}}>Utxos: {utxos.map(u => (<li style={{ overflow: 'hidden', textOverflow: 'ellipsis' }} key={u}>{u}</li>))}</ul>
+            <ul style={{width: '80%'}}>Utxos: {utxos?.map(u => (<li style={{ overflow: 'hidden', textOverflow: 'ellipsis' }} key={u}>{u}</li>))}</ul>
             <ul>balance: {balance}</ul>
             <ul style={{width: '80%'}}>UsedAddresses: {usedAddresses.map(addr => (<li style={{ overflow: 'hidden', textOverflow: 'ellipsis' }} key={addr}>{addr}</li>))}</ul>
             <ul style={{width: '80%'}}>UnusedAddresses: {unusedAddresses.map(addr => (<li style={{ overflow: 'hidden', textOverflow: 'ellipsis' }} key={addr}>{addr}</li>))}</ul>
             <ul style={{width: '80%'}}>rewardAddresses: {rewardAddresses.map(addr => (<li style={{ overflow: 'hidden', textOverflow: 'ellipsis' }} key={addr}>{addr}</li>))}</ul>
             <ul>changeAddress: {changeAddress}</ul>
-            <button onClick={() => signTransaction()}>SignTransaction</button>
+            {/* <button onClick={() => signTransaction()}>SignTransaction</button> */}
             <button onClick={() => signData()}>SignData</button>
           </div>
         ) : (

e0dee094066b2576f77aba64cefc6b5e3ce87b06
ByteZhang <quincysx@onekey.so>
2024-07-02 19:03:28 +0800

    chore: optimize timeout

--- packages/example/components/chains/suiStandard/params.ts
+++ packages/example/components/chains/suiStandard/params.ts
@@ -1,26 +1,26 @@
 export default {
   signMessage: [
     {
-      id: 'signMessage',
-      name: 'signMessage',
-      value: '010203',
-    },
-    {
       id: 'signMessage hello',
       name: 'signData Hello',
       value: '48656c6c6f204f6e654b6579',
     },
-  ],
-  signPersonalMessage: [
     {
-      id: 'signPersonalMessage',
-      name: 'signPersonalMessage',
+      id: 'signMessage',
+      name: 'signMessage',
       value: '010203',
     },
+  ],
+  signPersonalMessage: [
     {
       id: 'signPersonalMessage hello',
       name: 'signPersonalMessage Hello',
       value: '48656c6c6f204f6e654b6579',
+    },
+    {
+      id: 'signPersonalMessage',
+      name: 'signPersonalMessage',
+      value: '010203',
     },
   ],
   signTransaction: (address: string) => [

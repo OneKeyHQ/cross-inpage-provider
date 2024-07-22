1c9cf852ef8de274e11faeeabd06c4b567a83756
HuanXiang <huanxiangspace@qq.com>
2024-07-22 18:19:12 +0800

    feat: scdo provider

--- packages/providers/onekey-scdo-provider/src/OnekeyScdoProvider.ts
+++ packages/providers/onekey-scdo-provider/src/OnekeyScdoProvider.ts
@@ -35,6 +35,10 @@ export interface Tx {
   gasLimit?: number;
   timestamp?: number;
   payload?: string;
+  hash?: string;
+  signature?: {
+    Sig: string;
+  }
 }
 
 export interface RawTransaction {
@@ -58,16 +62,17 @@ export interface SignedTx {
 }
 
 type ScdoRequest = {
-  [ScdoRequestMethods.scdo_requestAccounts]: () => Promise<string[]>;
-  [ScdoRequestMethods.scdo_disconnect]: () => Promise<void>;
-  [ScdoRequestMethods.scdo_getAccounts]: () => Promise<string[]>;
-  [ScdoRequestMethods.scdo_getBalance]: (address: string) => Promise<string>;
-  [ScdoRequestMethods.scdo_signTransaction]: (tx: Tx) => Promise<SignedTx>;
-  [ScdoRequestMethods.scdo_estimateGas]: (tx: Tx) => Promise<string>;
-  [ScdoRequestMethods.scdo_sendTransaction]: (tx: Tx) => Promise<SignedTx>;
+  [ScdoRequestMethods.scdo_requestAccounts]: () => string[];
+  [ScdoRequestMethods.scdo_disconnect]: () => void;
+  [ScdoRequestMethods.scdo_getAccounts]: () => string[];
+  [ScdoRequestMethods.scdo_getBalance]: (address: string, blockHash?: string, blockHeight?: number) => string;
+  [ScdoRequestMethods.scdo_signTransaction]: (tx: Tx) => SignedTx;
+  [ScdoRequestMethods.scdo_estimateGas]: (tx: Tx) => string;
+  [ScdoRequestMethods.scdo_sendTransaction]: (tx: Tx) => SignedTx;
 }
 
 type ScdoRequestParams = Parameters<ScdoRequest[ScdoRequestMethods]>;
+type ScdoRequestResponse = ReturnType<ScdoRequest[ScdoRequestMethods]>;
 
 interface ScdoRequestProps {
   method: ScdoRequestMethods;
@@ -77,16 +82,14 @@ interface ScdoRequestProps {
 export type IProviderScdo = {
   on: <E extends keyof ScdoProviderEventsMap>(event: E, cb: ScdoProviderEventsMap[E]) => void;
   removeListener: <E extends keyof ScdoProviderEventsMap>(event: E, cb: ScdoProviderEventsMap[E]) => void;
-  request: (props: ScdoRequestProps) => ReturnType<ScdoRequest[ScdoRequestMethods]>;
+  request: (props: ScdoRequestProps) => Promise<ScdoRequestResponse>;
 };
 
 type JsBridgeRequest = {
   [K in keyof ScdoRequest]: (
-    ...params: Parameters<ScdoRequest[K]>
+    ...params: [void|string|RawTransaction|undefined|SignedTx]
   ) => Promise<TypeUtils.WireStringified<TypeUtils.ResolvePromise<ReturnType<ScdoRequest[K]>>>>;
 };
-
-type JsBridgeRequestParams<T extends keyof JsBridgeRequest> = Parameters<JsBridgeRequest[T]>;
 
 type JsBridgeRequestResponse<T extends keyof JsBridgeRequest> = ReturnType<JsBridgeRequest[T]>;
 
@@ -100,6 +103,7 @@ function isWalletEventMethodMatch({ method, name }: { method: string; name: stri
 
 export class ProviderScdo extends ProviderScdoBase implements IProviderScdo {
   private _account?: string;
+  public accounts: string[] = [];
 
   constructor(props: OneKeyScdoProviderProps) {
     super({
@@ -127,7 +131,7 @@ export class ProviderScdo extends ProviderScdoBase implements IProviderScdo {
 
   private _callBridge<T extends keyof JsBridgeRequest>(params: {
     method: T;
-    params: JsBridgeRequestParams<T>;
+    params: (void|string|RawTransaction|undefined|SignedTx)[];
   }): JsBridgeRequestResponse<T> {
     return this.bridgeRequest(params) as JsBridgeRequestResponse<T>;
   }
@@ -178,7 +182,43 @@ export class ProviderScdo extends ProviderScdoBase implements IProviderScdo {
     return super.removeListener(eventName, listener);
   }
 
-  request(props: ScdoRequestProps): ReturnType<ScdoRequest[ScdoRequestMethods]> {
-    return this._callBridge(props);
+  private _transformTx(tx: Tx): RawTransaction {
+    return {
+      Type: 0,
+      From: tx.from,
+      To: tx.to,
+      Amount: tx.amount ?? 0,
+      AccountNonce: tx.accountNonce ?? 0,
+      GasPrice: tx.gasPrice ?? 1,
+      GasLimit: tx.gasLimit ?? 0,
+      Timestamp: tx.timestamp ?? 0,
+      Payload: tx.payload ?? '',
+    };
+  }
+
+  async request(props: ScdoRequestProps) {
+    const reqParams = props.params as (void|string|undefined|Tx)[];
+    let params;
+    if (props.method === ScdoRequestMethods.scdo_estimateGas) {
+      const tx = reqParams[0] as Tx;
+      params = [{
+        Data: this._transformTx(tx),
+        Hash: tx.hash ?? '',
+        Signature: tx.signature ?? { Sig: '' },
+      }] as [SignedTx];
+    } else if (props.method === ScdoRequestMethods.scdo_signTransaction || props.method === ScdoRequestMethods.scdo_sendTransaction) {
+      params = [this._transformTx(props.params[0] as Tx)];
+    } else {
+      params = reqParams as [string];
+    }
+    const res = await this._callBridge({
+      method: props.method,
+      params,
+    });
+    if (props.method === ScdoRequestMethods.scdo_getAccounts) {
+      this.accounts.length = 0;
+      this.accounts.push(...res as string[]);
+    }
+    return res;
   }
 }

8d859a3eb52e3103000afe60af1f4a30d12334c5
ByteZhang <ByteZhang@protonmail.com>
2024-10-31 12:37:27 +0800

    chore: optimize eth example

--- packages/example/components/chains/nostr/params.ts
+++ packages/example/components/chains/nostr/params.ts
@@ -13,10 +13,20 @@ export default {
   ],
   signSchnorr: [
     {
+      id: 'signSchnorr-32HexString',
+      name: '32 位 Hex String',
+      value: '2118c65161c7d68b4bdbe1374f658532670057ab1bb0c99937d0ff7cff45cb5e',
+    },
+    {
       id: 'signSchnorr',
-      name: 'signSchnorr',
+      name: 'Hex String',
       value: '010203',
     },
+    // {
+    //   id: 'signSchnorr-text',
+    //   name: '普通文本',
+    //   value: 'some text here',
+    // }
   ],
   nip04encrypt: [
     {

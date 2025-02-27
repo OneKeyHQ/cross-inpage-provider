641d8f7b747b1a01732cc5a814948c150f28670b
Leon <lixiao.dev@gmail.com>
2025-02-27 20:21:14 +0800

    feat: Add callBridge

--- packages/providers/onekey-neo-provider/src/OneKeyNeoProvider.ts
+++ packages/providers/onekey-neo-provider/src/OneKeyNeoProvider.ts
@@ -2,16 +2,21 @@
 import { IInpageProviderConfig } from '@onekeyfe/cross-inpage-provider-core';
 import { getOrCreateExtInjectedJsBridge } from '@onekeyfe/extension-bridge-injected';
 import { ProviderNeoBase } from './ProviderNeoBase';
-import { INeoProviderMethods, INeoGetProviderResponse } from './types';
+import { INeoProviderMethods, INeoGetProviderResponse, NeoProviderEventsMap, JsBridgeRequest, JsBridgeRequestParams, JsBridgeRequestResponse, INeoNetworkResponse } from './types';
 import { versionInfo } from '@onekeyfe/cross-inpage-provider-core'
 
 const PROVIDER_EVENTS = {
+  'connect': 'connect',
   'disconnect': 'disconnect',
   'accountChanged': 'accountChanged',
   'message_low_level': 'message_low_level',
 } as const;
 
 export type PROVIDER_EVENTS_STRINGS = keyof typeof PROVIDER_EVENTS;
+
+function isWalletEventMethodMatch(method: string, name: string) {
+  return method === `metamask_${name}` || method === `wallet_events_${name}`;
+}
 
 /**
  * send NEOLine.N3.EVENT.READY event to notify the page that Neo Provider is ready
@@ -44,6 +49,10 @@ class NEOLineN3 {
     async getProvider(): Promise<INeoGetProviderResponse> {
       return this.provider.getProvider();
     }
+
+    async getNetworks(): Promise<INeoNetworkResponse> {
+      return this.provider.getNetworks();
+    }
   }
 }
 
@@ -56,6 +65,59 @@ class ProviderNeo extends ProviderNeoBase implements INeoProviderMethods {
       ...props,
       bridge: props.bridge || getOrCreateExtInjectedJsBridge({ timeout: props.timeout }),
     });
+    this._registerEvents();
+  }
+
+  private _registerEvents() {
+    window.addEventListener('onekey_bridge_disconnect', () => {
+      this._handleDisconnected();
+    });
+
+    this.on(PROVIDER_EVENTS.message_low_level, (payload) => {
+      const { method } = payload;
+      if (isWalletEventMethodMatch(method, PROVIDER_EVENTS.accountChanged)) {
+        this._handleAccountChange();
+      }
+    });
+  }
+
+  private _handleDisconnected(options: { emit: boolean } = { emit: true }) {
+    if (options.emit && this.isConnectionStatusChanged('disconnected')) {
+      this.emit('disconnect');
+      this.emit('accountChanged');
+    }
+  }
+
+  private _handleAccountChange() {
+    this.emit('accountChanged');
+  }
+
+  on<E extends keyof NeoProviderEventsMap>(
+    event: E,
+    listener: NeoProviderEventsMap[E]
+  ): this {
+    return super.on(event, listener);
+  }
+
+  off<E extends keyof NeoProviderEventsMap>(
+    event: E,
+    listener: NeoProviderEventsMap[E]
+  ): this {
+    return super.off(event, listener);
+  }
+
+  emit<E extends keyof NeoProviderEventsMap>(
+    event: E,
+    ...args: Parameters<NeoProviderEventsMap[E]>
+  ): boolean {
+    return super.emit(event, ...args);
+  }
+
+  private _callBridge<T extends keyof JsBridgeRequest>(params: {
+    method: T;
+    params?: JsBridgeRequestParams<T>;
+  }): JsBridgeRequestResponse<T> {
+    return this.bridgeRequest(params) as JsBridgeRequestResponse<T>;
   }
 
   getProvider(): Promise<INeoGetProviderResponse> {
@@ -65,6 +127,10 @@ class ProviderNeo extends ProviderNeoBase implements INeoProviderMethods {
       version: versionInfo.version || '1.0.0',
       compatibility: [],
     });
+  }
+
+  async getNetworks(): Promise<INeoNetworkResponse> {
+    return this._callBridge({ method: 'getNetworks' });
   }
 }
 
--- packages/providers/onekey-neo-provider/src/types.ts
+++ packages/providers/onekey-neo-provider/src/types.ts
@@ -1,3 +1,4 @@
+import type * as TypeUtils from './type-utils';
 export interface IAccount {
   address: string;
 }
@@ -9,6 +10,28 @@ export interface INeoGetProviderResponse {
   compatibility: string[]
 }
 
+export interface INeoNetworkResponse {
+  networks: string[];  // Array of network names the wallet provider has available
+  chainId: number;     // ChainId the wallet is currently set to
+  defaultNetwork: string; // Network the wallet is currently set to
+}
+
 export interface INeoProviderMethods {
   getProvider(): Promise<INeoGetProviderResponse>;
+  getNetworks(): Promise<INeoNetworkResponse>;
 }
+
+export interface NeoProviderEventsMap {
+  connect: () => void;
+  disconnect: () => void;
+  accountChanged: () => void;
+  message_low_level: (payload: { method: string; [key: string]: any }) => void;
+}
+
+export type JsBridgeRequest = {
+  [K in keyof INeoProviderMethods]: (params: Parameters<INeoProviderMethods[K]>[0]) => Promise<TypeUtils.WireStringified<TypeUtils.ResolvePromise<ReturnType<INeoProviderMethods[K]>>>>
+}
+
+export type JsBridgeRequestParams<T extends keyof JsBridgeRequest> = Parameters<JsBridgeRequest[T]>[0]
+
+export type JsBridgeRequestResponse<T extends keyof JsBridgeRequest> = ReturnType<JsBridgeRequest[T]>

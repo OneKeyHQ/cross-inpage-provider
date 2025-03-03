4b2fffea66eb0e243ec7687dbde92758718e638c
Leon <lixiao.dev@gmail.com>
2025-03-03 10:30:00 +0800

    feat: Add Neo provider (#312)
    * feat: Add neo package
    
    * feat: Inject neo provider
    
    * chore: type
    
    * feat: Add callBridge
    
    * feat: Add method
    
    * feat: Add method
    
    * feat: Add write methods
    
    * feat: Add Neo provider read methods
    
    * chore: update version

---
+++ packages/providers/onekey-neo-provider/src/type-utils.ts
@@ -0,0 +1,7 @@
+export type WireStringified<T> = T extends Array<infer P>
+  ? Array<WireStringified<P>>
+  : T extends object
+  ? { [K in keyof T]: WireStringified<T[K]> }
+  : T;
+
+export type ResolvePromise<T> = T extends Promise<infer P> ? P : T;

4239fb2f3e134a6fa7f1a514acdb651cddf0783e
ByteZhang <ByteZhang@protonmail.com>
2026-01-22 18:48:33 +0800

    fix: solona
    fix: solona
    
    fix: ui

--- packages/providers/inpage-providers-hub/src/injectWeb3Provider.ts
+++ packages/providers/inpage-providers-hub/src/injectWeb3Provider.ts
@@ -314,7 +314,7 @@ function injectWeb3Provider({
   });
 
   registerSolanaWallet(solana, {
-    icon: WALLET_CONNECT_INFO.onekey.icon as WalletIcon,
+    icon: WALLET_CONNECT_INFO.jupiter.icon as WalletIcon,
     name: 'Jupiter',
   });
 
